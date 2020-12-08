3152013bcd6890498da91c7c889c5d139bfe5394
gianni-tbz <75663431+gianni-tbz@users.noreply.github.com>
2020-12-08 11:34:01 +0100

    first design
    first design of game

---
+++ greenfoot-maze/player.java
@@ -0,0 +1,67 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class player here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class player extends Actor
+{
+    /**
+     * Act - do whatever the player wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+        checkKeyPress();
+        touchingWall();
+        win();
+    }
+    private void checkKeyPress()
+    {
+        if (Greenfoot.isKeyDown("d")) 
+        {
+            if(getX()+30 < 600){
+            setLocation(getX()+30, getY());
+            }
+            Greenfoot.delay(10);
+        }
+        
+        if (Greenfoot.isKeyDown("w")) 
+        {
+            if(getY()-30 > 0){
+            setLocation(getX(), getY()-30);
+            }
+            Greenfoot.delay(10);
+        }
+        if (Greenfoot.isKeyDown("a")) 
+        {
+            if(getX()-30 > 0){
+            setLocation(getX()-30, getY());
+            }      
+            Greenfoot.delay(10);
+        }
+        
+        if (Greenfoot.isKeyDown("s")) 
+        {
+            if(getY()+30 < 600){
+            setLocation(getX(), getY()+30);
+            }
+            Greenfoot.delay(10);
+        }
+    }   
+    public void touchingWall() 
+    {
+      if(isTouching(shortWallH.class) || isTouching(shortWallV.class)){            
+            setLocation(15, 585);
+        } 
+    }
+    public void win()
+    {
+      if(isTouching(goal.class)){
+            Greenfoot.stop();
+        }
+    }
+}
